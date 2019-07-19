b9794ed98ea8685b198fb6fc41ecbf104fe03423
Blank <blankngnl@protonmail.com>
2019-07-19 16:37:04 +0200

    CORS policy

--- app.js
+++ app.js
@@ -5,9 +5,29 @@ const config = require('./config');
 const setupController = require('./controllers/setupController');
 const apiController = require('./controllers/apiController');
 
+
 const port = process.env.PORT || 3000;
 
 app.use('/assets', express.static(__dirname + 'public'));
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 app.set('viewengine', 'ejs');
 
